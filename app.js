789260c724c4c9571f7449c502f5b8e0af34142b
tapia1013 <53988501+tapia1013@users.noreply.github.com>
2020-01-08 11:37:19 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,126 @@
+function isTouching(a, b) {
+  const aRect = a.getBoundingClientRect();
+  const bRect = b.getBoundingClientRect();
+
+  return !(
+    aRect.top + aRect.height < bRect.top ||
+    aRect.top > bRect.top + bRect.height ||
+    aRect.left + aRect.width < bRect.left ||
+    aRect.left > bRect.left + bRect.width
+  );
+}
+
+
+
+const avatar = document.querySelector('#player');
+const coin = document.querySelector('#coin')
+
+window.addEventListener('keyup', function (e) {
+
+  if (e.key === 'ArrowDown' || e.key === 'Down') {
+
+    const currTop = extractPos(avatar.style.top);
+
+    avatar.style.top = `${currTop + 50}px`
+
+    console.log(avatar.style.top);
+
+  } else if (e.key === 'ArrowUp' || e.key === 'Up') { // other direction
+    const currTop = extractPos(avatar.style.top);
+    avatar.style.top = `${currTop - 50}px`;
+
+  } else if (e.key === 'ArrowRight' || e.key === 'Right') { // other direction
+    const currLeft = extractPos(avatar.style.left);
+    avatar.style.left = `${currLeft + 50}px`;
+    avatar.style.transform = 'scale(1,1)'
+
+  } else if (e.key === 'ArrowLeft' || e.key === 'Left') { // other direction
+    const currLeft = extractPos(avatar.style.left);
+    avatar.style.left = `${currLeft - 50}px`;
+    avatar.style.transform = 'scale(-1,1)'
+  }
+
+  if (isTouching(avatar, coin)) moveCoin();
+});
+
+const extractPos = (pos) => {
+  if (!pos) return 100; // we put 100 cause 0 was making down go up
+
+
+  return parseInt(pos.slice(0, -2))
+}
+
+
+
+
+const moveCoin = () => {
+  const y = Math.floor(Math.random() * window.innerHeight);
+  const x = Math.floor(Math.random() * window.innerWidth);
+
+  coin.style.top = `${y}px`;
+  coin.style.left = `${x}px`;
+}
+
+
+moveCoin();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
